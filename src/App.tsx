ce3c148c5237d4546af3578ee19e81e62531c75a
Mikkel Meerwaldt Jørgensen <Mikkel_m_j@hotmail.com>
2024-02-12 09:40:04 +0100

    Added some more imports from Chakra ui and imported & customized a Grid template.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>Hello world</div>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`  
+          }}
+      >
+        <GridItem bg='orange.300' area={'nav'}>
+          Nav
+        </GridItem>
+        <Show above="lg">
+        <GridItem bg='pink.300' area={'aside'}>
+          Aside
+        </GridItem>
+        </Show>
+        <GridItem bg='green.300' area={'main'}>
+          Main
+        </GridItem>
+      </Grid>
     </div>
   )
 }

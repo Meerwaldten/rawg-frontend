4ed7eafb838c68cfff5b5020e9fe108e191733ba
Mikkel Meerwaldt Jørgensen <Mikkel_m_j@hotmail.com>
2024-02-12 10:37:09 +0100

    Added darkmode switch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,19 @@
+import { HStack, Switch, useColorMode, Text } from "@chakra-ui/react"
+
+const ColorModeSwitch = () => {
+    const {colorMode, toggleColorMode} = useColorMode();
+
+    return <HStack>
+        <Switch 
+            colorScheme="green" 
+            isChecked={colorMode === "dark"}
+            onChange={toggleColorMode}
+        >
+        </Switch>
+        <Text>Dark mode</Text>
+    </HStack>
+    
+
+};
+
+export default ColorModeSwitch;

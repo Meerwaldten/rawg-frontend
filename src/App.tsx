270862047c0603175f1e3cbf6d60e62fa3365a54
Mikkel Meerwaldt Jørgensen <Mikkel_m_j@hotmail.com>
2024-02-26 11:45:33 +0100

    Added a selector for Platforms.

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/usePlatforms";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
 
   return (
     <Grid
@@ -29,7 +32,12 @@ function App() {
         </GridItem>
       </Show>
       <GridItem gridArea={"main"}>
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector onSelectedPlatform={(platform) => setSelectedPlatform(platform)} 
+          selectedPlatform={selectedPlatform}
+        />
+        <GameGrid 
+          selectedGenre={selectedGenre} 
+          selectedPlatform={selectedPlatform}/>
       </GridItem>
     </Grid>
   );
